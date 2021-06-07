615c1bcbf420475fa360d8ab296eda4c2c22c64e
Omexae <antoine.ra@aol.com>
2021-06-07 17:01:43 +0200

    EXO1 FINI

--- Exo1/Part1.py
+++ Exo1/Part1.py
@@ -8,3 +8,16 @@ print(estPremier.premier(31))
 print(estParfait.parfait(6))
 print(estChanceux.chance(11))
 
+listchance = [0] * 998
+listparfait = [0] * 998
+
+for i in range(0, 998):
+    listchance[i] = estChanceux.chance(i+2)
+    listparfait[i] = estParfait.parfait(i+2)
+    print(i)
+print("Liste nombre chanceux de 2 à 1000")
+for j in range(0, 998):
+    print(listchance[j])
+print("Liste nombre parfaits de 2 à 998")
+for k in range(2, 998):
+    print(listparfait[k])
--- Exo1/estChanceux.py
+++ Exo1/estChanceux.py
@@ -4,6 +4,6 @@ import estPremier
 def chance(arg1):
     res = True
     for num in range(1, arg1):
-        if not estPremier.premier(arg1 + num + (num * num)):
+        if not estPremier.premier(arg1 + num + (num ** 2)):
             res = False
     return res
